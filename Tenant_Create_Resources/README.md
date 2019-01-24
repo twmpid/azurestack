ad44e2fb720edbafe3c7f15cdf0e71ac1cb54e0c
patrickkuan <patrickkuan@taiwanmobile.com>
2019-01-24 16:44:19 +0800

    create new template and update readme file

---
+++ Tenant_Create_Resources/README.md
@@ -0,0 +1,5 @@
+# Azure Stack
+請確認執行環境已安裝 Azure Stack Powershell, https://docs.microsoft.com/zh-tw/azure/azure-stack/azure-stack-powershell-install
+
+# Powershell 部署
+使用Powershell 部署,請參考create_resource_from_template_sample.ps1腳本. 部署時請使用新的資源群組Resource Group
---
+++ Tenant_Create_Resources/create_resource_from_template_sample.ps1
@@ -0,0 +1,20 @@
+﻿# Set Taiwanmobile Azure Stack API endpoint
+Add-AzureRmEnvironment -Name "TenantARM" -ARMEndpoint "https://management.twm.azurestack.taiwancloud.com.tw"
+# Login Azure Stack
+Login-AzureRmAccount -EnvironmentName "TenantARM" 
+# The file path your downloaded
+$templatePath = 'C:\Temp\Tenant_Create_Resources\create_multiple_ubuntu_vm_template.json'
+# Set deployment variables
+$vmprefix = "mtVMs" # Modify this per deployment
+$RGName = "yourRG" #Please change the RGname (Resource group name)
+$myLocation = "twm"
+
+# Create resource group for template deployment
+New-AzureRmResourceGroup -Name $RGName -Location $myLocation
+
+# Deploy simple IaaS template
+New-AzureRmResourceGroupDeployment `
+    -Name myDeployment$vmprefix `
+    -ResourceGroupName $RGName `
+    -TemplateFile $templatePath `
+    -ApiVersion "2018-03-01-hybrid"
